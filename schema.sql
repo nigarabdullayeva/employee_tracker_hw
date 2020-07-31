cb366952b91763178b82a6c312580191943390a3
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-07-31 12:57:12 -0500

    addind schema file

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_infoDB;
+
+CREATE database employee_infoDB;
+
+USE employee_infoDB;
+
+CREATE TABLE department(
+ id INT AUTO_INCREMENT NOT NULL,
+ name VARCHAR(30) NOT NULL,
+ PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+ id INT AUTO_INCREMENT NOT NULL,
+ title VARCHAR(30) NOT NULL,
+ salary DECIMAL NOT NULL,
+ department_id INT NOT NULL,
+ PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT default 0, 
+PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee
